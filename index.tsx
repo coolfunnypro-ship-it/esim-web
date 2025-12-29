7833903b524845f4d402daccb7d4b3d159b3bed1
coolfunnypro-ship-it <coolfunnypro@gmail.com>
2025-12-29 17:20:22 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -3,14 +3,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+console.log('eSimGlobal: App is initializing...');
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  console.error("Critical Error: Could not find root element to mount to");
+} else {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+  console.log('eSimGlobal: App mounted successfully.');
 }
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
